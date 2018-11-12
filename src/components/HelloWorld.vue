de9d8dc68bb888a213b2ec296913e9293a27529c
huipu <1003075979@qq.com>
2018-11-12 23:44:19 +0800

    皮

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App haha'
+      msg: 'Welcome to Your Vue.js App haha piyixia'
     }
   }
 }
